499b9a199b247bebe7bf39b85d495a81d334febf
Alan Signetti <alansignetti@gmail.com>
2019-11-26 16:14:20 -0300

    Update Empleado.java

--- Sistema Stock/src/modelo/Empleado.java
+++ Sistema Stock/src/modelo/Empleado.java
@@ -14,7 +14,7 @@ public class Empleado {
 	}
 	@Override
 	public String toString() {
-		return "Empleado [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", sucursal=" + sucursal
+		return "Empleado [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", sucursal=" + sucursal.getIdSucursal()
 				+ "]";
 	}
 	public String getNombre() {
